e9bcfa8f92c9545c79a339b2dcd0e55b1bd58788
Felipe Rocha Campos <fmrcampos@berkeley.edu>
2018-06-18 12:51:33 -0700

    added uinject DAG root script for reading from serial

--- visualization.py
+++ visualization.py
@@ -1,7 +1,7 @@
 from visual import *
 import numpy as np
 import random
-from math import sqrt, sin, cos, tan, radians, pi
+from math import sqrt, sin, cos, tan, atan, radians, pi
 
 ''' CLASSES '''
 
@@ -62,6 +62,8 @@ class Network:
 
     _id = 0
     Z_OFFSET = 0.5
+
+    MIMSY = { "def01" : 0 }
 
     def __init__(self, w, h, inputs=[]):
         self.w = w
@@ -152,108 +154,4 @@ class Network:
         else:
             network = cls(w, h, inputs)
 
-        # Turns off the default user spin and zoom and handles these functions itself.
-        # This gives more control to the program and addresses the problem that at the time of writing,
-        # Visual has a hidden user scaling variable that makes it impossible to force the camera position
-        # by setting range, if the user has already zoomed using the mouse.
-
-        network.scene.userzoom = False
-        network.scene.userspin = False
-        rangemin = 1
-        rangemax = 100
-
-        i_x = 0
-        i_y = 0
-
-        updating = False
-
-        brk = False
-        _rate = 100
-
-        while brk == False:
-            rate(_rate)
-            if network.scene.kb.keys:
-                k = network.scene.kb.getkey()
-                _rate = 100
-
-                change = 15
-
-                if k == 'i':
-                    cx, cy, cz = network.center
-                    network.scene.center = (cx,cy,cz)
-                    network.scene.forward = (0,0,-1)
-                elif k == '1':
-                    network.scene.forward = (1,0,-.25)
-                elif k == '2':
-                    network.scene.forward = (0,1,-1)
-                elif k == '3':
-                    network.scene.forward = (1,0,0)
-                elif k == '4':
-                    network.scene.forward = (0,-1,0)
-                elif k == 'shift+down' and network.scene.range.x < rangemax:
-                    network.scene.range = network.scene.range.x + .5
-                elif k == 'shift+up' and network.scene.range.x > rangemin:
-                    network.scene.range = network.scene.range.x - .5
-                elif k == 'up':
-                    network.scene.center = (network.scene.center.x, \
-                        network.scene.center.y + .1, network.scene.center.z)
-                elif k == 'down':
-                    network.scene.center = (network.scene.center.x, \
-                        network.scene.center.y - .1, network.scene.center.z)
-                elif k == 'right':
-                    network.scene.center = (network.scene.center.x + .1, \
-                         network.scene.center.y, network.scene.center.z)
-                elif k == 'left':
-                    network.scene.center = (network.scene.center.x - .1, \
-                        network.scene.center.y, network.scene.center.z)
-                elif k == 'shift+left':
-                    network.scene.center = (network.scene.center.x, \
-                        network.scene.center.y, network.scene.center.z + .1)
-                elif k == 'shift+right':
-                    network.scene.center = (network.scene.center.x, \
-                        network.scene.center.y, network.scene.center.z - .1)
-                elif k == 'w':
-                    network.scene.forward = (network.scene.forward.x, \
-                        network.scene.forward.y - .1, network.scene.forward.z)
-                elif k == 's':
-                    network.scene.forward = (network.scene.forward.x, \
-                        network.scene.forward.y + .1, network.scene.forward.z)
-                elif k == 'a':
-                    network.scene.forward = (network.scene.forward.x - .1, \
-                        network.scene.forward.y, network.scene.forward.z)
-                elif k == 'd':
-                    network.scene.forward = (network.scene.forward.x + .1, \
-                        network.scene.forward.y, network.scene.forward.z)
-                elif k == 'A':
-                    network.scene.forward = (network.scene.forward.x, \
-                        network.scene.forward.y, network.scene.forward.z - .1)
-                elif k == 'D':
-                    network.scene.forward = (network.scene.forward.x, \
-                        network.scene.forward.y, network.scene.forward.z + .1)
-                elif k == '.' or k == 'q':
-                    brk = True
-                elif k == 'r':
-                    new_input = [[roll + change, pitch, 0] for [roll, pitch, _] in network.inputs]
-                    if new_input[0][0] <= 90:
-                        network.update(new_input)
-                elif k == 'p':
-                    new_input = [[roll, pitch + change, 0] for [roll, pitch, _] in network.inputs]
-                    if new_input[0][1] <= 90:
-                        network.update(new_input)
-                elif k == 'R':
-                    new_input = [[roll - change, pitch, 0] for [roll, pitch, _] in network.inputs]
-                    if new_input[0][0] >= -90:
-                        network.update(new_input)
-                elif k == 'P':
-                    new_input = [[roll, pitch - change, 0] for [roll, pitch, _] in network.inputs]
-                    if new_input[0][1] >= -90:
-                        network.update(new_input)
-
-        window.delete_all()
-        exit()
-
         return network
-
-''' VISUALIZATION '''
-
-network = Network.initialize()
